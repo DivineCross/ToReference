c5fb2fcec408b5163fd524ed1ae58e6a505bd5e5
DivineCross <DivineCross@users.noreply.github.com>
2024-12-05 14:26:37 +0800

    First commit

---
+++ js/helper.js
@@ -0,0 +1,145 @@
+((global, factory) => {
+    global.Helper = factory();
+})(typeof window === typeof undefined ? this : window, () => {
+    class Alphabetical {
+    }
+
+    Alphabetical.compare = (s1 = "", s2 = "") => {
+        const CharOrderMap = (() => {
+            const order = [
+                "0123456789",
+                `!"#$%&'()*+,-./`,
+                "AÀÁÂÃÄÅĀĂĄ",
+                "aàáâãäåāăą",
+                "BɃƁƂ",
+                "bƀɓƃ",
+                "CÇĆĈĊČƇ",
+                "cçćĉċčƈ",
+                "DĎĐƊƋ",
+                "dďđɗƌ",
+                "EÈÉÊËĒĔĖĘĚ",
+                "eèéêëēĕėęě",
+                "FƑ",
+                "fƒ",
+                "GĜĞĠĢƓ",
+                "gĝğġģɠ",
+                "HĤĦ",
+                "hĥħ",
+                "IÌÍÎÏĨĪĬĮİƗȽ",
+                "iìíîïĩīĭįıɨƚ",
+                "JĴ",
+                "jĵ",
+                "KĶƘ",
+                "kķƙ",
+                "LĹĻĽĿŁ",
+                "lĺļľŀł",
+                "M",
+                "m",
+                "NÑŃŅŇƝȠ",
+                "nñńņňɲƞ",
+                "OÒÓÔÕÖØŌŎŐƟƠ",
+                "oòóôõöøōŏőɵơ",
+                "PƤ",
+                "pƥ",
+                "Q",
+                "q",
+                "RŔŖŘ",
+                "rŕŗř",
+                "SŚŜŞŠ",
+                "sśŝşš",
+                "TŢŤŦƬƮ",
+                "tţťŧƫƭʈ",
+                "UÙÚÛÜŨŪŬŮŰŲƯ",
+                "uùúûüũūŭůűųư",
+                "VƲ",
+                "vʋ",
+                "WŴ",
+                "wŵ",
+                "X",
+                "x",
+                "YÝŸŶƳ",
+                "yýÿŷƴ",
+                "ZŹŻŽƵ",
+                "zźżžƶ",
+                "ÆæĲĳŒœ",
+                "ÐðÞþßĸŊŋſ",
+                "ƄƅƆƉƍƎƏƐƔƕƖƛƜƢƣƦƧƨƩƪƱƷƸƹƺƻƼƽƾƿ",
+            ].join("");
+
+            const map = new Map;
+            for (let i = 0; i < order.length; ++i)
+                map.set(order[i], i);
+
+            return map;
+        })();
+
+        const compareChar = (c1 = "", c2 = "") => {
+            const o1 = CharOrderMap.get(c1);
+            const o2 = CharOrderMap.get(c2);
+
+            return (o1 === undefined || o2 === undefined)
+                ? c1.charCodeAt(0) - c2.charCodeAt(0)
+                : o1 - o2;
+        };
+
+        const ignoreRegex = /[ ()（）,;\-\.]/g;
+        const a = s1.replace(ignoreRegex, "");
+        const b = s2.replace(ignoreRegex, "");
+
+        let r = 0;
+        for (let i = 0; i < Math.min(a.length, b.length); ++i)
+            if ((r = compareChar(a[i], b[i])) !== 0)
+                return r;
+
+        return a.length - b.length;
+    };
+
+    class Dom {
+    }
+
+    /**
+     * @param {HTMLInputElement} element
+     * @param {(text: string) => void} textHandler
+     */
+    Dom.registerTextFileChange = (element, textHandler) => {
+        element.addEventListener("change", event => {
+            const file = element.files[0];
+
+            if (!file)
+                return;
+
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.addEventListener("load", () => {
+                textHandler(reader.result);
+            });
+        });
+    };
+
+    /**
+     * @param {HTMLElement} element
+     * @param {boolean} keepSelect
+     */
+    Dom.copyElement = (element, keepSelect = false) => {
+        const range = document.createRange();
+        range.selectNodeContents(element);
+
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        element.focus();
+        document.execCommand("copy");
+
+        if (!keepSelect)
+            selection.removeAllRanges();
+    }
+
+    class Helper {
+    }
+
+    Helper.Alphabetical = Alphabetical;
+    Helper.Dom = Dom;
+
+    return Helper;
+});
